979a1a2797da854b1ea57604a971c0dedf770556
Rdandamanda <danulamatous@seznam.cz>
2025-09-02 23:37:47 +0200

    Fixed it and showing off to mum She likes it, had a good idea and I'm happy how rapidly I could add those changes

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from tkinter import *
 from tkinter import ttk
 
 class Source:
-    generated_segments = []
+    def __init__(self):
+        self.generated_segments = []
     def generate_segments(self, parent_screen):
         for interactor in parent_screen.ray_interactors:
             print(interactor.collide())
@@ -17,10 +18,10 @@ class Glass_Rectangle(Interactor):
 class Screen:
     ray_sources = []
     ray_interactors = []
-    tk_frame = Frame()
-    tk_canvas = Canvas(master=tk_frame, bg="red", width=20, height=20) #TODO: is this going to be class-wide or per-instance?
     def __init__(self):
-        pass
+        self.tk_frame = Frame()
+        self.tk_canvas = Canvas(master=self.tk_frame, bg="#DDDDDD", width=700, height=400)
+        self.tk_canvas.grid()
     def solve_collisions(self):
         for source in self.ray_sources:
             return f"solve collision for source {source}"
@@ -35,10 +36,16 @@ root.title("Laser visualiser tool")
 ntb_Screens = ttk.Notebook()
 ntb_Screens.grid()
 
+#The debug screen
 if load_debug_screen == True:
-    pass
-    #startup_Screen = Screen()
-    #ntb_Screens.add(startup_Screen.tk_frame, text="Debug Screen")
+    startup_Screen = Screen()
+    ntb_Screens.add(startup_Screen.tk_frame, text="Debug Screen")
+    startup_Screen1 = Screen()
+    startup_Screen1.tk_canvas.configure(bg="yellow")
+    ntb_Screens.add(startup_Screen1.tk_frame, text="Další plocha")
+    startup_Screen2 = Screen()
+    startup_Screen2.tk_canvas.configure(bg="lavender")
+    ntb_Screens.add(startup_Screen2.tk_frame, text="Plocha 3")
 
 #startup_Screen.ray_sources.append(Source())
 #startup_Screen.ray_interactors.append(Glass_Rectangle())
