ccfa3a11d9db972903d5bb3e9217d1cc72adc260
Rdandamanda <danulamatous@seznam.cz>
2025-09-10 22:28:52 +0200

    Rushed push Quick push that changes rendering. Broken but in progress

--- main.py
+++ main.py
@@ -17,13 +17,13 @@ class Source:
     def generate_segments(self, parent_screen):
         self.generated_segments.append(Segment(self.x, self.y, self.angle))
         for interactor in parent_screen.ray_interactors:
-            print(interactor.collide(self))
+            print(interactor.collide(self)) #TODO: There is nothing here
 
 class Interactor:
     def collide(self, ray_source):
-        return f"Collision of a ray {ray_source} with interactor {self}"
+        return f"Collision of a ray {ray_source} with interactor {self}" #TODO: There is literally nothing here, also this should be a __string__() function
 
-class Glass_Rectangle(Interactor):
+class Glass_Rectangle(Interactor): #TODO: Decide how such objects will be stored
     pass
 
 class Screen:
@@ -36,28 +36,36 @@ class Screen:
     def solve_collisions(self):
         for source in self.ray_sources:
             source.generate_segments(self)
-            return f"solve collision for source {source}"
+            #TODO Add some debug print statements maybe
     def draw_all(self):
         for source in self.ray_sources:
-            for segment in source.generated_segments:
-                print(f"Drawing line for segment {segment}")
-                self.tk_canvas.create_line(segment.start_x, segment.start_y, segment.start_x+100, segment.start_y*(1+tan(radians(segment.angle))), fill="black")
+            generated_segments = source.generated_segments
+            for i in range(len(generated_segments) - 1): #All but the final segment have a next segment to latch onto as their end point
+                self.tk_canvas.create_line(generated_segments[i].start_x, generated_segments[i].start_y, generated_segments[i+1].start_x, generated_segments[i+1].start_y)
+            #Finally, the final segment is handled seperately
 
 load_debug_screen = True
 load_extra_debug_screens = True
 
 root = Tk()
-root.title("Laser visualiser tool")
+root.title("Ray optics tool")
 
 ntb_Screens = ttk.Notebook()
 ntb_Screens.grid()
 
 #The debug screen
 if load_debug_screen == True:
+    #Tab setup
     startup_Screen = Screen()
     ntb_Screens.add(startup_Screen.tk_frame, text="Debug Screen")
 
-#Stuff I was showing to mum. New screens, differentiated by colour. Proud of the high ease of adding them
+    #Populating it with objects
+    startup_Screen.ray_sources.append(Source(100, 100, 22.5))
+    #startup_Screen.ray_interactors.append(Glass_Rectangle())
+    startup_Screen.solve_collisions()
+    startup_Screen.draw_all()
+
+#Stuff for showing off the tabs. New screens, differentiated by colour. Happy with the high ease of adding them
 if load_extra_debug_screens == True:
     startup_Screen1 = Screen()
     startup_Screen1.tk_canvas.configure(bg="yellow")
@@ -65,10 +73,5 @@ if load_extra_debug_screens == True:
     startup_Screen2 = Screen()
     startup_Screen2.tk_canvas.configure(bg="lavender")
     ntb_Screens.add(startup_Screen2.tk_frame, text="Plocha 3")
-
-startup_Screen.ray_sources.append(Source(100, 100, 22.5))
-startup_Screen.ray_interactors.append(Glass_Rectangle())
-startup_Screen.solve_collisions()
-startup_Screen.draw_all()
 
 root.mainloop()
