357f7c02a721a22e163755ea4f7619b105f896a3
F100cTomas <145491617+F100cTomas@users.noreply.github.com>
2025-12-10 21:05:00 +0100

    Mirror

--- interactor.py
+++ interactor.py
@@ -14,7 +14,7 @@ class Interactor:
         self._border_width = border_width
 
     def draw(self, canvas: Canvas) -> None:
-        canvas.create_rectangle(self._left, self._top, self._right, self._bottom, fill=self._fill, outline=self._outline, width=self._border_width)
+        canvas.create_rectangle(self._left, self._bottom, self._right, self._top, fill=self._fill, outline=self._outline, width=self._border_width)
 
     def collision_check(self, source: Source) -> tuple[int, int] | None:
         return source.collide_box(self._left, self._top, self._right, self._bottom)
@@ -30,10 +30,28 @@ class Interactor:
                 wall_antinormal = (wall_antinormal + pi) % (2 * pi)
                 alpha = (ray_angle - wall_antinormal) % (2 * pi)
                 beta = asin(sin(alpha) * self._index)
-            except:
-                return []
+            except ValueError as e:
+                if str(e) != 'math domain error':
+                    raise e
+                beta = (pi - alpha) % (2 * pi)
+                final_angle = ((beta + wall_antinormal) * (180 / pi)) % 360
+                return [Source(x, y, source._angle, source._color, source._depth  - 1)]
         else:
             beta = asin(sin(alpha) / self._index)
+        final_angle = ((beta + wall_antinormal) * (180 / pi)) % 360
+        return [Source(x, y, final_angle, source._color, source._depth  - 1)]
+
+class Mirror(Interactor):
+    def __init__(self, left: int, top: int, right: int, bottom: int, fill: str = "#9bc8a1", outline: str = "#aad9e3", border_width: int = 2) -> None:
+        super().__init__(left, top, right, bottom, 0, fill, outline, border_width)
+
+    def collide(self, source: Source, x: int, y: int) -> list[Source]:
+        if source._depth <= 1:
+            return []
+        ray_angle = source._angle * (pi / 180)
+        wall_antinormal = 0 if x == self._right else (1 / 2) * pi if y == self._bottom else pi if x == self._left else (3 / 2) * pi
+        alpha = (ray_angle - wall_antinormal) % (2 * pi)
+        beta = (pi - alpha) % (2 * pi)
         final_angle = ((beta + wall_antinormal) * (180 / pi)) % 360
         return [Source(x, y, final_angle, source._color, source._depth  - 1)]
 
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/bin/python3
 from source import Source
-from interactor import Interactor
+from interactor import Interactor, Mirror
 from screen import Screen
 from tkinter import Tk, Frame, Canvas, ttk
 from math import tan, radians
@@ -22,10 +22,9 @@ def main() -> None:
     # Tab setup
     startup_Screen = Screen(ntb_Screens, "Startup")
     # Populating it with objects
-    for n in range(16):
-        startup_Screen.add_source(Source(300, 140, (180 + 22.5 * n) % 360))
-    startup_Screen.add_interactor(Interactor(100, 400, 700, 550, 1.5))
-    startup_Screen.add_interactor(Interactor(750, 200, 1000, 900, 2.0))
+    for n in range(0, 720, 10):
+        startup_Screen.add_source(Source(300, 140, (n / 2) % 360, "red", 4))
+    startup_Screen.add_interactor(Mirror(100, 250, 1200, 300))
 
     startup_Screen.draw_all()
 
